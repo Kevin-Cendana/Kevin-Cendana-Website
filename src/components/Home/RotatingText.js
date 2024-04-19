3ecedb382814430e28eef72b7a1609799d633c28
KevCendana <kevincendana@csus.edu>
2024-04-18 19:00:46 -0700

    Contact: Colors
    - Contact: Colors changed for profile picture
    - Class assigned to home-typing-alias

--- src/components/Home/RotatingText.js
+++ src/components/Home/RotatingText.js
@@ -170,7 +170,7 @@ class ReactRotatingText extends React.Component {
         } = this.props;
 
         return (
-            <span style={{ color }} {...other}>
+            <span className="home-typing-alias"style={{ color }} {...other}>
             { this.state.output }
             { cursor ? <span className="react-rotating-text-cursor">|</span> : null }
             </span>
